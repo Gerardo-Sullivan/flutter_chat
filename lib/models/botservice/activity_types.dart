397bd053a3eb7f74aaec971a2e6a5a78610d84bf
Gerardo Sullivan <em12664@aut.ac.nz>
2019-05-01 13:10:00 +1200

    Move file

---
+++ lib/models/botservice/activity_types.dart
@@ -0,0 +1,17 @@
+class ActivityTypes {
+  static const String Message = "message";
+  static const String ContactRelationUpdate = "contactRelationUpdate";
+  static const String ConversationUpdate = "conversationUpdate";
+  static const String Typing = "typing";
+  static const String EndOfConversation = "endOfConversation";
+  static const String Event = "event";
+  static const String Invoke = "invoke";
+  static const String DeleteUserData = "deleteUserData";
+  static const String MessageUpdate = "messageUpdate";
+  static const String MessageDelete = "messageDelete";
+  static const String InstallationUpdate = "installationUpdate";
+  static const String MessageReaction = "messageReaction";
+  static const String Suggestion = "suggestion";
+  static const String Trace = "trace";
+  static const String Handoff = "handoff";
+}
